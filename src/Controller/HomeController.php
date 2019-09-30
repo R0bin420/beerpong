f78699d390bb26bb67e0ae410bca7604e861c3bd
Robin <robin.lousee@gmail.com>
2019-09-30 15:56:26 +0200

    Start game

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
+use App\Entity\GameUser;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -38,11 +40,15 @@ class HomeController extends AbstractController
             return $this->redirectToRoute('home');
         }
 
+        // Schritt 1. Variablen deklarieren
         $error = "";
+
+        // Schritt2. Aus Anfrage Daten holen
         $username = $request->get('username');
         $password = $request->get('password');
         $passwordRepeat = $request->get('password_repeat');
 
+        // Schritt 3. Prüfen der gegebenen Variablen
         if ($request->getMethod() == 'POST') {
             if (empty($username) || empty($password)) {
                 $error = "Nutzername oder Passwort ist nicht gefüllt.";
@@ -54,6 +60,8 @@ class HomeController extends AbstractController
                     if ($password != $passwordRepeat) {
                         $error = "Passwörter stimmen nicht überein";
                     } else {
+
+                        // Schritt 4. Eintragen in die Datenbank
                         $newUser = new User();
                         $newUser
                             ->setPassword($encoder->encodePassword($newUser,$password))
@@ -63,6 +71,7 @@ class HomeController extends AbstractController
                         $manager->persist($newUser);
                         $manager->flush();
 
+                        // Scritt 5. Weiterleiten auf richtige Seite
                         return $this->redirectToRoute('app_login', ['successRegister' => true]);
                     }
                 }
@@ -73,12 +82,24 @@ class HomeController extends AbstractController
             "error" => $error
         ]);
     }
+
     /**
      * @Route("/2versus2", name="2versus2")
+     * @param EntityManagerInterface $manager
+     * @param Request $request
      * @return Response
      */
-    public function twoversus() {
-        return $this->render('home/2versus2.html.twig');
+    public function twoversus(
+        EntityManagerInterface $manager,
+        Request $request
+    ) {
+        $users = $manager->getRepository(User::class)->findBy([], ['username' => 'ASC']);
+
+        var_dump($request->get('team1'));
+        //die();
+        return $this->render('home/2versus2.html.twig', [
+            "users" => $users
+        ]);
     }
 
     /**
@@ -92,20 +113,69 @@ class HomeController extends AbstractController
     /**
      * @Route("/1versus1", name="1versus1")
      * @param EntityManagerInterface $manager
+     * @param Request $request
      * @return Response
+     * @throws \Exception
      */
     public function oneversus(
-        EntityManagerInterface $manager
-    ) {
-        $users = $manager->getRepository(User::class)->findBy([],['username' => 'ASC']);
+        EntityManagerInterface $manager,
+        Request $request
+    )
+    {
+        /** @var User[] $users */
+        $users = $manager->getRepository(User::class)->findBy([], ['username' => 'ASC']);
+
+        // Schritt 1. Variablen deklarieren
+        $error = "";
+        // Schritt 2. Aus Anfrage Daten holen
+        $team1_user = $manager->getRepository(User::class)->find((int)$request->get("team1_player1"));
+        $team2_user = $manager->getRepository(User::class)->find((int)$request->get("team2_player1"));
+
         if ($request->getMethod() == 'POST') {
+            if ($team1_user instanceof User && $team2_user instanceof User)  {
 
+                if($team1_user != $team2_user)  {
+
+                    $game = new Game();
+                    $game->setStartDate(new \DateTime());
+                    $game->setWinnerTeam(1);
+
+                    $manager->persist($game);
+
+                    $gameUser1 = new GameUser();
+                    $gameUser1->setUser($team1_user);
+                    $gameUser1->setGame($game);
+                    $gameUser1->setTeam(1);
+
+                    $manager->persist($gameUser1);
+
+
+                    $gameUser2 = new GameUser();
+                    $gameUser2->setUser($team2_user);
+                    $gameUser2->setGame($game);
+                    $gameUser2->setTeam(2);
+
+                    $manager->persist($gameUser2);
+
+                    $manager->flush();
+
+                }
+                else {
+                    echo "Man kann nicht gegensich selbst spielen";
+                }
+
+            }
+            else {
+                echo "Personen wurden nicht gefunden";
+            }
         }
+        
 
-        return $this->render('home/1versus1.html.twig',[
+        return $this->render('home/1versus1.html.twig', [
             "users" => $users
         ]);
     }
+
 
     /**
      * @Route("/finish", name="finish")
